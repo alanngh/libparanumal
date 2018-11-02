02a79726fed0570812d8580f8ed6bcf345ba3a04
alanngh <31671843+alanngh@users.noreply.github.com>
2018-11-02 14:02:35 -0400

    Add files via upload
    adding new printinf format easy to plot in matlab

--- solvers/elliptic/src/PCG.c
+++ solvers/elliptic/src/PCG.c
@@ -115,8 +115,18 @@ int pcg(elliptic_t* elliptic, dfloat lambda,
     // ]
     occaTimerToc(mesh->device,"Residual update");
     
-    if (options.compareArgs("VERBOSE", "TRUE")&&(mesh->rank==0)) 
-      printf("CG: it %d r norm %12.12f alpha = %f \n",Niter, sqrt(rdotr1), alpha);
+    
+    int Matlab = 0;
+    Matlab = options.getArgs("MATLAB",Matlab);   // get from setup file  
+    
+    
+    if (options.compareArgs("VERBOSE", "TRUE")&&(mesh->rank==0)){ 
+		if (!Matlab)
+			printf("CG: it %d r norm %12.12f alpha = %f \n",Niter, sqrt(rdotr1), alpha);
+		else
+			printf(" %12.12f , \n",sqrt(rdotr1));
+    }
+     
 
     if(rdotr1 < TOL) {
       rdotr0 = rdotr1;
